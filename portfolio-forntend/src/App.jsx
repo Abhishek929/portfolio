dd3444dc997fef72c81508cb9390c8d354181bbb
Abhishek929 <abhishek00004917@gmail.com>
2025-08-19 15:48:27 +0530

    Add files App.jsx

--- portfolio-forntend/src/App.jsx
+++ portfolio-forntend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import UsersPage from './components/UsersPage';
 import SignupForm from './components/SignupForm';
 import LoginForm from './components/LoginForm';
 import Logout from "./pages/Logout";
@@ -11,6 +10,15 @@ import ForgotPassword from "./components/ForgotPassword";
 import ResetPasswordForm from './components/ResetPasswordForm';
 import AdminRoutes from './Routes/AdminRoutes';
 import AccountPage from './user/AccountPage';
+import AdminPage from "./admin/AdminPage";
+import ManageUsers from "./admin/ManageUsers";
+import SiteSettings from "./admin/SiteSettings";
+import Reports from "./admin/Reports";
+import ContactsPage from "./admin/ContactsPage";
+import EditUser from './admin/EditUser';
+import NotificationsPage from './admin/NotificationsPage';
+import ProfilePage from './user/ProfilePage';
+import EditProfilePage from './user/EditProfilePage';
 
 function App() {
   
@@ -28,6 +36,17 @@ function App() {
           <Route path="/logout" element={<Logout />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPasswordForm />} />
+
+          { /* Admin routes are handled in AdminRoutes.jsx, so no need to define them here */}
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/admin/manage-users" element={<ManageUsers />} />
+          <Route path="/admin/site-settings" element={<SiteSettings />} />
+          <Route path="/admin/reports" element={<Reports />} />
+          <Route path="/admin/contacts" element={<ContactsPage />} />
+          <Route path="/admin/edit-user/:id" element={<EditUser />} />
+          <Route path="/admin/profile" element={<ProfilePage />} />
+          <Route path="/admin/edit-profile/:id" element={<EditProfilePage />} />
+          <Route path="/admin/notifications" element={<NotificationsPage />} />
         </Routes>
       </Router>
     </>
