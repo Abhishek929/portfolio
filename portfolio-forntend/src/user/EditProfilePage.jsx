f468175c472de468451bbc0ebfac19fc85bb4046
Abhishek929 <abhishek00004917@gmail.com>
2025-08-19 16:49:26 +0530

    Add files auth routes.

--- portfolio-forntend/src/user/EditProfilePage.jsx
+++ portfolio-forntend/src/user/EditProfilePage.jsx
@@ -77,7 +77,7 @@ export default function EditProfilePage() {
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(formData),
+          body: formData,
         }
       );
 
