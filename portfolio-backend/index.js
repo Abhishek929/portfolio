0b877b5f542cb6196114974a1a58953607b6e459
Abhishek929 <abhishek00004917@gmail.com>
2025-08-19 16:36:42 +0530

    Add files index js.

--- portfolio-backend/index.js
+++ portfolio-backend/index.js
@@ -5,6 +5,7 @@ import authRouter from "./Routes/authRoutes.js";
 import connectDB from "./Config/db_connect.js";
 import cors from "cors";
 import contactRoutes from "./Routes/contactRoutes.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
@@ -16,7 +17,6 @@ app.use(cors({
     credentials: true, // if you need cookies/authorization headers
 }));
 
-
 app.use(express.json());
 
 // Connect to DB
@@ -26,6 +26,7 @@ connectDB();
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/contact", contactRoutes);
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Portfolio Backend API");
