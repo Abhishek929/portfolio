115de659705847c42cc973429e0a7a5360af5e08
Abhishek929 <abhishek00004917@gmail.com>
2025-08-19 16:32:13 +0530

    Add files auth routes and index js.

--- portfolio-backend/index.js
+++ portfolio-backend/index.js
@@ -5,7 +5,6 @@ import authRouter from "./Routes/authRoutes.js";
 import connectDB from "./Config/db_connect.js";
 import cors from "cors";
 import contactRoutes from "./Routes/contactRoutes.js";
-import path from "path";
 dotenv.config();
 
 const app = express();
@@ -13,17 +12,10 @@ const PORT = process.env.PORT || 8080;
 
 // CORS Middleware
 app.use(cors({
-    origin: "https://portfolio-abhi-six.vercel.app",
-    credentials: true,
+    origin: "https://portfolio-abhi-six.vercel.app", // no trailing slash
+    credentials: true, // if you need cookies/authorization headers
 }));
 
-// Debug ke liye
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://portfolio-abhi-six.vercel.app");
-    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
 
 app.use(express.json());
 
@@ -34,7 +26,6 @@ connectDB();
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/contact", contactRoutes);
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Portfolio Backend API");
