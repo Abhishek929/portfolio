86362f109378d07a93474fc6b5a46b8c05aad283
Abhishek929 <abhishek00004917@gmail.com>
2025-08-20 15:10:41 +0530

    Add update some files

--- portfolio-forntend/src/components/Navbar.jsx
+++ portfolio-forntend/src/components/Navbar.jsx
@@ -22,7 +22,6 @@ const Navbar = () => {
         <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
         <Link to="/about-us" className={location.pathname === '/about-us' ? 'active' : ''}>About</Link>
         <Link to="/services" className={location.pathname === '/services' ? 'active' : ''}>Our Services</Link>
-        <Link to="/userspage" className={location.pathname === '/userspage' ? 'active' : ''}>Users Page</Link>
         {loggedIn ? (<Link to="/logout" className={location.pathname === '/logout' ? 'active' : ''}>Log out</Link>) : (
             <Link to="/login" className={location.pathname === '/login' ? 'active' : ''}>Login</Link>)}
       </div>
