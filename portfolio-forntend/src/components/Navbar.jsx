b2b020b2991830ed2145cbfe1fc34ac8b40aed14
Abhishek929 <abhishek00004917@gmail.com>
2025-08-20 19:01:53 +0530

    Add update some files.

--- portfolio-forntend/src/components/Navbar.jsx
+++ portfolio-forntend/src/components/Navbar.jsx
@@ -8,6 +8,17 @@ const Navbar = () => {
 
   const loggedIn = Boolean(localStorage.getItem('token'));
 
+  let role = null;
+  try {
+    const userData = localStorage.getItem('user');
+    if (userData) {
+      const parsed = JSON.parse(userData);
+      role = parsed.role; // role should be saved in localStorage when login
+    }
+  } catch (err) {
+    console.error("Error parsing user from localStorage", err);
+  }
+
   return (
     <nav className="navbar">
       <div className="navbar-left">
@@ -22,6 +33,17 @@ const Navbar = () => {
         <Link to="/" className={location.pathname === '/' ? 'active' : ''}>Home</Link>
         <Link to="/about-us" className={location.pathname === '/about-us' ? 'active' : ''}>About</Link>
         <Link to="/services" className={location.pathname === '/services' ? 'active' : ''}>Our Services</Link>
+        {role === "user" && (
+          <Link to="/my-account" className={location.pathname === '/my-account' ? 'active' : ''}>
+            My Account
+          </Link>
+        )}
+
+        {role === "admin" && (
+          <Link to="/admin/profile" className={location.pathname === '/admin/profile' ? 'active' : ''}>
+            Admin Profile
+          </Link>
+        )}
         {loggedIn ? (<Link to="/logout" className={location.pathname === '/logout' ? 'active' : ''}>Log out</Link>) : (
             <Link to="/login" className={location.pathname === '/login' ? 'active' : ''}>Login</Link>)}
       </div>
