2964c4bdab00cd74d08ff976584f0299d3592d1a
Abhishek929 <abhishek00004917@gmail.com>
2025-08-21 16:44:20 +0530

    Add update some files.

--- portfolio-backend/Middlewares/multer.js
+++ portfolio-backend/Middlewares/multer.js
@@ -1,7 +1,18 @@
 import multer from "multer";
+import path from "path";
 
-const storage = multer.memoryStorage(); // keep in memory, no /uploads
+// configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // folder to store files
+  },
+  filename: (req, file, cb) => {
+    // use timestamp + original extension
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
+// multer instance
 const upload = multer({ storage });
 
 export default upload;
